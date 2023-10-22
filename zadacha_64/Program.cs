7c3349283752cf36c368eea8da7127f165104486
Artur Frolov <art.frolov2479@gmail.com>
2023-10-22 21:12:35 +0300

    second commit

--- zadacha_64/Program.cs
+++ zadacha_64/Program.cs
@@ -1,2 +1,20 @@
-﻿System.Console.Write("M = ");
-int m = Convert.ToInt32(Console.ReadLine());
+﻿static void PrintNaturalNumbers(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(n);
+        if (n > 1)
+        {
+            Console.Write(", ");
+        }
+        PrintNaturalNumbers(n - 1);
+    }
+}
+
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine());
+PrintNaturalNumbers(N);
