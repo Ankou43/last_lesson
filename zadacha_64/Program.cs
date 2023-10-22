17cce02ccd3ecb8f4ef3a7e910fcd7e3e4ff9de3
Artur Frolov <art.frolov2479@gmail.com>
2023-10-22 21:04:12 +0300

    first commit

---
+++ zadacha_64/Program.cs
@@ -0,0 +1,2 @@
+﻿System.Console.Write("M = ");
+int m = Convert.ToInt32(Console.ReadLine());
---
+++ zadacha_66/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void suma (int m, int n)
+{
+    int sum = 0;
+    for (int i = m; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+    System.Console.WriteLine("Сумма натуральных элементов в промежутке от M до N: " + sum);
+}
+
+suma (m, n);
